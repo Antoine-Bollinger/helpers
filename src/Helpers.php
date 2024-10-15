d2aaa2076a25d19587bb09308893f5b5fa3c47de
Antoine Bollinger <antoine.bollinger@gmail.com>
2024-10-15 08:03:52 +0200

    📝 licence

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the Abollinger\Helpers package.
+ *
+ * (c) Antoine Bollinger <antoine.bollinger@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Abollinger;
 
 use \Symfony\Component\Yaml\Yaml;
--- src/Parsedown.php
+++ src/Parsedown.php
@@ -1,4 +1,13 @@
-<?php
+<?php 
+/*
+ * This file is part of the Abollinger\Helpers package.
+ *
+ * (c) Antoine Bollinger <antoine.bollinger@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Abollinger;
 
 final class Parsedown extends \Parsedown
